462682c847702ed447896c74eec783bb04c747f3
Joona Yoon <joonas.yoon@gmail.com>
2021-12-09 21:07:03 +0900

    fix: set migration directory

--- migrate.py
+++ migrate.py
@@ -1,9 +1,12 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-migrate = Migrate(app, db)
+MIGRATION_DIR = os.path.join('models', 'migrations')
+
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
